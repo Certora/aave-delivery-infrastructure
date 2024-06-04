260d150c4868ed841e84aaaf88208daddc18be2b
nisnislevi <nissan@certora.com>
2024-06-04 12:14:59 +0300

    the new shuffle rules are running

--- security/certora/harness/CrossChainForwarderHarness.sol
+++ security/certora/harness/CrossChainForwarderHarness.sol
@@ -12,6 +12,7 @@ contract CrossChainForwarderHarness is CrossChainForwarder {
     bool _bridgeTransaction_was_called = false;
     bytes32 _envelopeId;
     bytes32 _transactionId;
+    uint256 _destinationChainId;
 
     uint256 _last_nonceTX_sent;
     uint256 _max_TXnonce;  // the maximal TX nonce ever sent
@@ -20,7 +21,11 @@ contract CrossChainForwarderHarness is CrossChainForwarder {
 
     // mapping from TX-id to TX-nonce + 1.
     // Hence TXid was sended if and only if _TXid_2_TXnonceP1[TXid]>0
-    mapping(bytes32 => uint) _TXid_2_TXnonceP1; 
+    mapping(bytes32 => uint) _TXid_2_TXnonceP1;
+
+    address[] _active_bridges_dest;
+    address[] _active_bridges_curr;
+    uint256 _num_of_active_bridges;
 
     constructor(
                 ForwarderBridgeAdapterConfigInput[] memory bridgeAdaptersToEnable,
@@ -34,7 +39,7 @@ contract CrossChainForwarderHarness is CrossChainForwarder {
     /* ==============================================================================
        The contract CrossChainForwarder sends out TX, by emiting them. Since CVL can't
        look at the emit data, we use the following function to "capture" information
-       about sended TX.
+       about the sended TX.
        We also maintain additional information that doesn't exist in the storage
        of CrossChainForwarder.
        ============================================================================*/
@@ -49,6 +54,7 @@ contract CrossChainForwarderHarness is CrossChainForwarder {
         _bridgeTransaction_was_called = true;
         _envelopeId = envelopeId;
         _transactionId = transactionId;
+        _destinationChainId = destinationChainId;
         _last_nonceTX_sent = get_nonceTX_from_encodedT(encodedTransaction);
         if (_last_nonceTX_sent > _max_TXnonce) 
             _max_TXnonce = _last_nonceTX_sent;
@@ -65,7 +71,11 @@ contract CrossChainForwarderHarness is CrossChainForwarder {
             require (_TXid_2_TXnonceP1[transactionId] == _last_nonceTX_sent+1);
             require (_TXid_2_ENid[transactionId] == envelopeId);
         }
-
+        _num_of_active_bridges = bridgeAdapters.length;
+        for (uint256 i = 0; i < bridgeAdapters.length; i++) {
+          _active_bridges_dest[i] = bridgeAdapters[i].destinationBridgeAdapter;
+          _active_bridges_curr[i] = bridgeAdapters[i].currentChainBridgeAdapter;
+        }
 
         bool ret_val = super._bridgeTransaction
             (envelopeId, transactionId, encodedTransaction, destinationChainId,
@@ -79,10 +89,20 @@ contract CrossChainForwarderHarness is CrossChainForwarder {
         _bridgeTransaction_was_called = false;
         _envelopeId = 0;
         _transactionId = 0;
+        _destinationChainId = 0;
         _last_nonceTX_sent = 0;
     }
 
+    function get_active_bridges_dest() external view returns (address[] memory) {
+        return _active_bridges_dest;
+    }
+    function get_active_bridges_curr() external view returns (address[] memory) {
+        return _active_bridges_curr;
+    }
 
+    function get_num_of_active_bridges() external view returns (uint) {
+        return _num_of_active_bridges;
+    }
     function get_max_TXnonce() external view returns (uint) {
         return _max_TXnonce;
     }
@@ -97,6 +117,9 @@ contract CrossChainForwarderHarness is CrossChainForwarder {
     }
     function get_param_transactionId() external view returns (bytes32) {
         return _transactionId;
+    }
+    function get_param_destinationChainId() external view returns (uint256) {
+      return _destinationChainId;
     }
     function get_last_nonceTX_sent() external view returns (uint256) {
         return _last_nonceTX_sent;
