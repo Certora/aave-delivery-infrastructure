465e354f7622af25e4605d2b216f2b04e90b3b57
nisnislevi <nissan@certora.com>
2024-06-05 13:52:00 +0300

    add sanity

--- security/certora/scripts/CrossChainForwarder-run-all.sh
+++ security/certora/scripts/CrossChainForwarder-run-all.sh
@@ -1,25 +1,16 @@
 
-certoraRun --send_only \
-           --fe_version latest \
-           security/certora/confs/verifyCrossChainForwarder-sanity.conf 
+certoraRun security/certora/confs/verifyCrossChainForwarder-sanity.conf 
 
-certoraRun --send_only \
-           --fe_version latest \
-           security/certora/confs/verifyCrossChainForwarder-envelopRetry.conf
+certoraRun security/certora/confs/verifyCrossChainForwarder-envelopRetry.conf
 
-certoraRun --send_only \
-           --fe_version latest \
-           security/certora/confs/verifyCrossChainForwarder-newEnvelope.conf
+certoraRun security/certora/confs/verifyCrossChainForwarder-newEnvelope.conf
 
-certoraRun --send_only \
-           --fe_version latest \
-           security/certora/confs/verifyCrossChainForwarder-simpleRules.conf
+certoraRun security/certora/confs/verifyCrossChainForwarder-simpleRules.conf
 
-certoraRun --send_only \
-           --fe_version latest \
-           security/certora/confs/verifyCrossChainForwarder-invariants.conf
+certoraRun security/certora/confs/verifyCrossChainForwarder-invariants.conf
 
-certoraRun --send_only \
-           --fe_version latest \
-           security/certora/confs/verifyCrossChainForwarder-encode-decode-correct.conf \
+certoraRun security/certora/confs/verifyCrossChainForwarder-encode-decode-correct.conf \
            --rule encode_decode_well_formed_TX
+
+certoraRun security/certora/confs/verifyCrossChainForwarder-shuffle.conf
+
